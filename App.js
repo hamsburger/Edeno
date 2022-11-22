8b38dc50471d77179ef3428adc957a77e8b042e3
Nurin Fazil <nurinfazil@Nurins-MacBook-Air.local>
2022-11-22 00:47:38 -0500

    Playing around. Added colour theme

--- App.js
+++ App.js
@@ -1,18 +1,59 @@
-
-import React from 'react';
-import { NativeBaseProvider, extendTheme } from 'native-base';
-import { Content } from './Content';
+import React from "react";
+import { NativeBaseProvider, extendTheme } from "native-base";
+import { Content } from "./Content";
 
 export default () => {
   const { colors } = extendTheme({});
 
   const theme = extendTheme({
     colors: {
-      // Add new color
-      primary: colors.gray,
-      // Redefining only one shade, rest of the color will remain same.
-      amber: {
-        400: '#d97706',
+      edeno_green: {
+        0: "#597F51",
+      },
+      vermillion_red: {
+        0: "#B9422C",
+      },
+      light_goldenrod_yellow: {
+        0: "#EFF7D5",
+      },
+      pale_spring_bud: {
+        0: "#EFF7BC",
+      },
+      russet_brown: {
+        0: "#754824",
+      },
+      bistro_brown: {
+        0: "#432D1E",
+      },
+      cultured_grey: {
+        0: "#EEF0F2",
+      },
+      silver: {
+        0: "#C6C7C4",
+      },
+      spanish_grey: {
+        0: "#A0999E",
+      },
+      deep_tuape: {
+        0: "#806B6B",
+      },
+      app_bg: {
+        0: "#FBFBFB",
+      },
+      secondary_green: {
+        0: "#72A077",
+      },
+      attention_yellow: {
+        0: "#C9B500",
+      },
+      menu_selected_dark_green: {
+        0: "#1B461A",
+      },
+      light_vermillion_red: {
+        0: "#F5CFC7",
+      },
+      faded_green: {
+        0: "#ADCDB",
       },
       // Configure breakpoints if needed. I copied from
       // default breakpoints: https://docs.nativebase.io/breakpoints
@@ -22,11 +63,7 @@ export default () => {
         md: 768,
         lg: 992,
         xl: 1280,
-      }
-    },
-    config: {
-      // Changing initialColorMode to 'dark'
-      initialColorMode: 'dark',
+      },
     },
   });
 
@@ -35,4 +72,4 @@ export default () => {
       <Content />
     </NativeBaseProvider>
   );
-}
+};
--- Content.js
+++ Content.js
@@ -1,49 +1,88 @@
-
-
-import React from 'react';
-import { Menu, Button, Text, HamburgerIcon, Container, Center} from 'native-base';
-import { Pressable, StyleSheet } from 'react-native'
+import React from "react";
+import {
+  Menu,
+  Button,
+  Text,
+  HamburgerIcon,
+  Container,
+  Center,
+  useColorMode,
+  useColorModeValue,
+} from "native-base";
+import { Pressable, StyleSheet } from "react-native";
 
 function Content() {
+  const bg = useColorModeValue("blue.300", "black");
+  const colourTheme = useColorModeValue("Dark", "Light");
 
-    // const { arr } = ["Aria", ]
-    return <Center>
-        <Menu
-            bg="white"
-            placement="bottom"
-            offset={30}
-            w={1300}
-            shadow={0}
-            padding={0}
-            trigger={(triggerProps) => {
-                return <Button 
-                        bg="white"
-                        h="100"
-                        w="1200"
-                        _pressed={
-                            {bg : "primary.200"}
-                        }
-                        {...triggerProps}
-                        >
-                            <HamburgerIcon top="4" size="7"/>
-                        </Button>
-            }}
-        >
-            <Menu.Item bg="blue.300" _pressed={ {bg : "blue.500"} }><Text>Aria</Text></Menu.Item>
-            <Menu.Item bg="green.300" _pressed={ {bg : "green.500"} }>Nunito Sans</Menu.Item>
-            <Menu.Item bg="yellow.300" _pressed={ {bg : "yellow.500"} }>Roboto</Menu.Item>
-        </Menu>
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  // const { arr } = ["Aria", ]
+  return (
+    <Center>
+      <Menu
+        bg="white"
+        placement="bottom"
+        offset={30}
+        w={1300}
+        shadow={0}
+        padding={0}
+        trigger={(triggerProps) => {
+          return (
+            <Button
+              bg="white"
+              h="100"
+              w="1200"
+              _pressed={{ bg: "primary.200" }}
+              {...triggerProps}
+            >
+              <HamburgerIcon top="4" size="7" />
+            </Button>
+          );
+        }}
+      >
+        <Menu.Item bg={bg} _pressed={{ bg: "blue.500" }}>
+          <Text
+            _dark={{ color: "edeno_green.0" }}
+            _light={{ color: "coolGray.800" }}
+          >
+            Aria
+          </Text>
+        </Menu.Item>
+        <Menu.Item bg={bg} _pressed={{ bg: "green.500" }}>
+          <Text
+            _dark={{ color: "warmGray.50" }}
+            _light={{ color: "coolGray.800" }}
+          >
+            Aria
+          </Text>
+        </Menu.Item>
+        <Menu.Item bg={bg} _pressed={{ bg: "yellow.500" }}>
+          <Text
+            _dark={{ color: "warmGray.50" }}
+            _light={{ color: "coolGray.800" }}
+          >
+            Aria
+          </Text>
+        </Menu.Item>
+        <Menu.Item bg={bg} _pressed={{ bg: "yellow.500" }}>
+          <Button onPress={toggleColorMode} h={10}>
+            {colourTheme}
+          </Button>
+        </Menu.Item>
+      </Menu>
     </Center>
+  );
 }
 
 const styles = StyleSheet.create({
-    /**
-     * Stylesheet styles take precedence over utility-first styles (Prop styles you see configured
-     * above).
-     */
-    menu: {
-        backgroundColor: "green"
-    }
+  /**
+   * Stylesheet styles take precedence over utility-first styles (Prop styles you see configured
+   * above).
+   */
+  menu: {
+    backgroundColor: "green",
+  },
 });
 
 export { Content };
