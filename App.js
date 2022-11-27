f4c796454752d6871a79b684a87b5319da9c4b4d
Nurin Fazil <nurinfazil@hotmail.com>
2022-11-27 17:11:55 -0500

    Dev nurin (#2)
    * Playing around. Added colour theme
    
    * Set up app structure for pages, components and fonts. Header FE done but no functionality

--- App.js
+++ App.js
@@ -1,38 +1,95 @@
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+import { NativeBaseProvider, extendTheme } from "native-base";
+import { Home } from "./pages/home/Home";
 
-import React from 'react';
-import { NativeBaseProvider, extendTheme } from 'native-base';
-import { Content } from './Content';
+import useFonts from "./hooks/useFont";
 
-export default () => {
+export default function App() {
   const { colors } = extendTheme({});
 
   const theme = extendTheme({
     colors: {
-      // Add new color
-      primary: colors.gray,
-      // Redefining only one shade, rest of the color will remain same.
-      amber: {
-        400: '#d97706',
+      edeno_green: {
+        0: "#597F51",
+      },
+      vermillion_red: {
+        0: "#B9422C",
+      },
+      light_goldenrod_yellow: {
+        0: "#EFF7D5",
+      },
+      pale_spring_bud: {
+        0: "#EFF7BC",
+      },
+      russet_brown: {
+        0: "#754824",
+      },
+      bistro_brown: {
+        0: "#432D1E",
+      },
+      cultured_grey: {
+        0: "#EEF0F2",
+      },
+      silver: {
+        0: "#C6C7C4",
+      },
+      spanish_grey: {
+        0: "#A0999E",
+      },
+      deep_tuape: {
+        0: "#806B6B",
+      },
+      app_bg: {
+        0: "#FBFBFB",
+      },
+      secondary_green: {
+        0: "#72A077",
+      },
+      attention_yellow: {
+        0: "#C9B500",
+      },
+      menu_selected_dark_green: {
+        0: "#1B461A",
+      },
+      light_vermillion_red: {
+        0: "#F5CFC7",
+      },
+      faded_green: {
+        0: "#ADCDB",
       },
       // Configure breakpoints if needed. I copied from
       // default breakpoints: https://docs.nativebase.io/breakpoints
-      breakpoints: {
-        base: 0,
-        sm: 480,
-        md: 768,
-        lg: 992,
-        xl: 1280,
-      }
     },
-    config: {
-      // Changing initialColorMode to 'dark'
-      initialColorMode: 'dark',
+    breakpoints: {
+      base: 0,
+      sm: 480,
+      md: 768,
+      lg: 992,
+      xl: 1280,
     },
   });
 
+  const [IsReady, SetIsReady] = useState(false);
+
+  const LoadFonts = async () => {
+    await useFonts();
+  };
+
+  if (!IsReady) {
+    return (
+      <AppLoading
+        startAsync={LoadFonts}
+        onFinish={() => SetIsReady(true)}
+        onError={() => {}}
+      />
+    );
+  }
+
   return (
     <NativeBaseProvider theme={theme}>
-      <Content />
+      <Home />
     </NativeBaseProvider>
   );
 }
